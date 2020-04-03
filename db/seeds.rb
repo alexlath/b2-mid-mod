7d428636de9787ed1457115ff2d1410a6a853998
Alex Latham <alexlthm@gmail.com>
2020-04-03 10:37:12 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+mechanic1 = Mechanic.create(
+  name:       'Sam Mills',
+  experience: 10)
+mechanic2 = Mechanic.create(
+  name:       'Kara Smith',
+  experience: 11)
+park = Park.create(
+  name:   'Hershey Park',
+  price:  '$50.00')
+lightning_racer = park.rides.create(
+  name: 'Lightning Racer',
+  rating: 8)
+storm_runner = park.rides.create(
+  name: 'Storm Runner',
+  rating: 7.9)
+the_great_bear = park.rides.create(
+  name: 'The Great Bear',
+  rating: 7.5)
+
