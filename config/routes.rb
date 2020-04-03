6312f7f9926c3e7fd0498fc57bf6edeb86764da6
Alex Latham <alexlthm@gmail.com>
2020-04-03 11:55:48 -0600

    Add show page and route for mechanic show and ride mechanic form

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see
 
   # Mechanics
-  get '/mechanics',     to: 'mechanics#index'
-  get '/mechanics/:id', to: 'mechanics#show'
+  get   '/mechanics',           to: 'mechanics#index'
+  get   '/mechanics/:id',       to: 'mechanics#show'
 
   # Parks
-  get '/parks/:id',     to: 'parks#show'
+  get   '/parks/:id',           to: 'parks#show'
+
+  # Rides
+  post  '/mechanics/:mechanic_id/rides', to: 'rides#update' 
 end
