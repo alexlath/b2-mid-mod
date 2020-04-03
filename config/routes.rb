e7d292c14e1c10eb03e1d06434988255ff1c45fc
Alex Latham <alexlthm@gmail.com>
2020-04-03 10:57:14 -0600

    Add mechanics show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see
 
   # Mechanics
-  get '/mechanics', to: 'mechanics#index'
+  get '/mechanics',     to: 'mechanics#index'
+  get '/mechanics/:id', to: 'mechanics#show'
 
   # Parks
-  get '/parks/:id', to: 'parks#show'
+  get '/parks/:id',     to: 'parks#show'
 end
